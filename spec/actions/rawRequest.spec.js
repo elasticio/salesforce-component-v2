4c9513a74231a9d716ad16deaad5c1cd6bb17b70
Oleg Oshurkov <olegosh137@gmail.com>
2021-07-28 02:24:05 +0300

    Added rawRequest action

---
+++ spec/actions/rawRequest.spec.js
@@ -0,0 +1,96 @@
+/* eslint-disable camelcase */
+const chai = require('chai');
+const nock = require('nock');
+
+const { expect } = chai;
+const common = require('../../lib/common.js');
+const testCommon = require('../common.js');
+const processAction = require('../../lib/actions/rawRequest');
+
+describe('Raw request test', () => {
+  beforeEach(() => {
+    nock(process.env.ELASTICIO_API_URI)
+      .get(`/v2/workspaces/${process.env.ELASTICIO_WORKSPACE_ID}/secrets/${testCommon.secretId}`)
+      .times(4)
+      .reply(200, testCommon.secret);
+  });
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  describe('Should succeed with method GET resource sobjects', () => {
+    it('Retrieves the list of queryable sobjects', async () => {
+      const scope = nock(testCommon.instanceUrl)
+        .get(`/services/data/v${common.globalConsts.SALESFORCE_API_VERSION}/sobjects`)
+        .reply(200, {
+          sobjects: [{
+            id: 382766725678,
+          }],
+        });
+
+      const msg = { body: { method: 'GET', path: 'sobjects' } };
+      const cfg = { secretId: testCommon.secretId };
+
+      const result = await processAction.process.call(this, msg, cfg);
+      expect(result.body).to.have.own.property('sobjects');
+
+      scope.done();
+    });
+  });
+
+  describe('Should succeed with method POST request create record', () => {
+    it('Retrieves the list of limits', async () => {
+      const scope = nock(testCommon.instanceUrl)
+        .get(`/services/data/v${common.globalConsts.SALESFORCE_API_VERSION}/limits`)
+        .reply(200, {
+          ActiveScratchOrgs: [{
+            Limit: { description: 'ActiveScratchOrgs' },
+          }],
+        });
+
+      const msg = { body: { method: 'GET', path: 'limits' } };
+      const cfg = { secretId: testCommon.secretId };
+
+      const result = await processAction.process.call(this, msg, cfg);
+      expect(result.body).to.have.own.property('ActiveScratchOrgs');
+
+      scope.done();
+    });
+  });
+
+  describe('Should succeed with method GET resource limits', () => {
+    it('Retrieves the list of limits', async () => {
+      const scope = nock(testCommon.instanceUrl)
+        .post(`/services/data/v${common.globalConsts.SALESFORCE_API_VERSION}/sobjects/Account/`)
+        .reply(200, {
+          id: '3498989438',
+          success: true,
+          errors: [],
+        });
+
+      const msg = { body: { method: 'POST', path: 'sobjects/Account/' } };
+      const cfg = { secretId: testCommon.secretId };
+
+      const result = await processAction.process.call(this, msg, cfg);
+      expect(result.body).to.have.own.property('id');
+      expect(result.body.success).to.be.deep.eql(true);
+
+      scope.done();
+    });
+  });
+
+  describe('Should fail', () => {
+    it('Failing', async () => {
+      const msg = { body: { method: 'GET', path: 'sobjects' } };
+      const cfg = { secretId: testCommon.secretId };
+
+      try {
+        await processAction.process.call(context, msg, cfg, () => {
+          throw new Error('Test case does not expect success response');
+        });
+      } catch (e) {
+        expect(e.message).to.be.contains('Request failed with status code 400');
+      }
+    });
+  });
+});
